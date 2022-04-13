437ec9e29d3c0bd986256b57e42e28b203741743
karupanerura <karupa@cpan.org>
2022-04-13 10:20:45 +0900

    mod

--- app.go
+++ app.go
@@ -42,46 +42,52 @@ func Run(ctx context.Context, botToken string, serverID string, outDir string) e
 		case <-ctx.Done():
 			return nil
 		case req := <-mentionHandler.StartRecChan:
-			func(req RecordingRequest) {
-				ctx := contextWithSignal(ctx, req.DisconnectionChan)
+			err = recordVoiceChannel(ctx, req, discord, serverID, outDir)
+			if err != nil {
+				req.Complete(err)
+				continue
+			}
 
-				vc, err := discord.ChannelVoiceJoin(serverID, req.VoiceChannelID, true, false)
-				if err != nil {
-					log.Error().Err(err).Msgf("failed to connect to voice channel %q", req.VoiceChannelID)
-					req.Complete(err)
-					return
-				}
-				defer func() {
-					err := vc.Disconnect()
-					if err != nil {
-						log.Error().Err(err).Msg("failed to disconnect from voice channel")
-					}
-				}()
+			req.Println("start finalizing...")
+			mixer := NewAudioMixer(filepath.Join(outDir, req.FilePrefix))
+			err = mixer.Mixdown(ctx)
+			if err != nil {
+				log.Error().Err(err).Msgf("failed to mixdown: %v", err)
+				req.Complete(err)
+				continue
+			}
 
-				log.Info().Msg("start recording...")
-				recorder := NewRecorder(filepath.Join(outDir, req.FilePrefix))
-				err = recorder.Record(ctx, vc)
-				if err != nil {
-					log.Error().Err(err).Msgf("failed to recording: %v", err)
-					req.Complete(err)
-					return
-				}
-				req.Println("finish recording!")
-
-				req.Println("start finalizing...")
-				mixer := NewAudioMixer(filepath.Join(outDir, req.FilePrefix))
-				err = mixer.Mixdown(ctx)
-				if err != nil {
-					log.Error().Err(err).Msgf("failed to mixdonw: %v", err)
-					req.Complete(err)
-					return
-				}
-
-				req.Println("complete! download from: https://team-nyanco.preview.karupas.org/polsvoice/"+req.FilePrefix+"-mix.wav")
-				req.Complete(nil)
-			}(req)
+			req.Println("complete! download from: https://team-nyanco.preview.karupas.org/polsvoice/" + req.FilePrefix + "-mix.wav")
+			req.Complete(nil)
 		}
 	}
+}
+
+func recordVoiceChannel(ctx context.Context, req RecordingRequest, discord *discordgo.Session, serverID, outDir string) error {
+	ctx = contextWithSignal(ctx, req.DisconnectionChan)
+
+	vc, err := discord.ChannelVoiceJoin(serverID, req.VoiceChannelID, true, false)
+	if err != nil {
+		log.Error().Err(err).Msgf("failed to connect to voice channel %q", req.VoiceChannelID)
+		return err
+	}
+	defer func() {
+		err := vc.Disconnect()
+		if err != nil {
+			log.Error().Err(err).Msg("failed to disconnect from voice channel")
+		}
+	}()
+
+	log.Info().Msg("start recording...")
+	recorder := NewRecorder(filepath.Join(outDir, req.FilePrefix))
+	err = recorder.Record(ctx, vc)
+	if err != nil {
+		log.Error().Err(err).Msgf("failed to recording: %v", err)
+		return err
+	}
+	req.Println("finish recording!")
+
+	return nil
 }
 
 func contextWithSignal(ctx context.Context, ch chan struct{}) context.Context {
--- go.mod
+++ go.mod
@@ -5,15 +5,15 @@ go 1.18
 require (
 	github.com/bwmarrin/dgvoice v0.0.0-20210225172318-caaac756e02e
 	github.com/bwmarrin/discordgo v0.23.2
+	github.com/karupanerura/riffbin v0.0.6
 	github.com/karupanerura/wavebin v0.0.4
 	github.com/rs/zerolog v1.25.0
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 )
 
 require (
 	github.com/gorilla/websocket v1.4.0 // indirect
-	github.com/karupanerura/riffbin v0.0.6 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
 	layeh.com/gopus v0.0.0-20210501142526-1ee02d434e32 // indirect
 )
